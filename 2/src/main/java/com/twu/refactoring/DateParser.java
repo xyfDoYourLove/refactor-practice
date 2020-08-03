7c34cce752a6d2f973c084a66aec077d433e577a
DoYourLove <xyf15620168540@163.com>
2020-08-03 21:04:58 +0800

    [XiaoYiFan] feat: refactoring 2

--- 2/src/main/java/com/twu/refactoring/DateParser.java
+++ 2/src/main/java/com/twu/refactoring/DateParser.java
@@ -6,6 +6,21 @@ import java.util.HashMap;
 import java.util.TimeZone;
 
 public class DateParser {
+    public static final String YEAR_STRING_EXCEPTION = "Year string is less than 4 characters";
+    public static final String YEAR_NUMBER_EXCEPTION = "Year is not an integer";
+    public static final String YEAR_CHECK_EXCEPTION = "Year cannot be less than 2000 or more than 2012";
+    public static final String MONTH_STRING_EXCEPTION = "Month string is less than 2 characters";
+    public static final String MONTH_NUMBER_EXCEPTION = "Month is not an integer";
+    public static final String MONTH_CHECK_EXCEPTION = "Month cannot be less than 1 or more than 12";
+    public static final String DATE_STRING_EXCEPTION = "Date string is less than 2 characters";
+    public static final String DATE_NUMBER_EXCEPTION = "Date is not an integer";
+    public static final String DATE_CHECK_EXCEPTION = "Date cannot be less than 1 or more than 31";
+    public static final String HOUR_STRING_EXCEPTION = "Hour string is less than 2 characters";
+    public static final String HOUR_NUMBER_EXCEPTION = "Hour is not an integer";
+    public static final String HOUR_CHECK_EXCEPTION = "Hour cannot be less than 0 or more than 23";
+    public static final String MINUTE_STRING_EXCEPTION = "Minute string is less than 2 characters";
+    public static final String MINUTE_NUMBER_EXCEPTION = "Minute is not an integer";
+    public static final String MINUTE_CHECK_EXCEPTION = "Minute cannot be less than 0 or more than 59";
     private final String dateAndTimeString;
     private static final HashMap<String, TimeZone> KNOWN_TIME_ZONES = new HashMap<String, TimeZone>();
 
@@ -29,65 +44,24 @@ public class DateParser {
     public Date parse() {
         int year, month, date, hour, minute;
 
-        try {
-            String yearString = dateAndTimeString.substring(0, 4);
-            year = Integer.parseInt(yearString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Year string is less than 4 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Year is not an integer");
-        }
-        if (year < 2000 || year > 2012)
-            throw new IllegalArgumentException("Year cannot be less than 2000 or more than 2012");
+        year = getNumFromDataAndTimeString(0, 4, YEAR_STRING_EXCEPTION, YEAR_NUMBER_EXCEPTION);
+        checkData(year, 2000, 2012, YEAR_CHECK_EXCEPTION);
 
-        try {
-            String monthString = dateAndTimeString.substring(5, 7);
-            month = Integer.parseInt(monthString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Month string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Month is not an integer");
-        }
-        if (month < 1 || month > 12)
-            throw new IllegalArgumentException("Month cannot be less than 1 or more than 12");
+        month = getNumFromDataAndTimeString(5, 7, MONTH_STRING_EXCEPTION, MONTH_NUMBER_EXCEPTION);
+        checkData(month, 1, 12, MONTH_CHECK_EXCEPTION);
 
-        try {
-            String dateString = dateAndTimeString.substring(8, 10);
-            date = Integer.parseInt(dateString);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("Date string is less than 2 characters");
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Date is not an integer");
-        }
-        if (date < 1 || date > 31)
-            throw new IllegalArgumentException("Date cannot be less than 1 or more than 31");
+        date = getNumFromDataAndTimeString(8, 10, DATE_STRING_EXCEPTION, DATE_NUMBER_EXCEPTION);
+        checkData(date, 1, 31, DATE_CHECK_EXCEPTION);
 
         if (dateAndTimeString.substring(11, 12).equals("Z")) {
             hour = 0;
             minute = 0;
         } else {
-            try {
-                String hourString = dateAndTimeString.substring(11, 13);
-                hour = Integer.parseInt(hourString);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new IllegalArgumentException("Hour string is less than 2 characters");
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Hour is not an integer");
-            }
-            if (hour < 0 || hour > 23)
-                throw new IllegalArgumentException("Hour cannot be less than 0 or more than 23");
+            hour = getNumFromDataAndTimeString(11, 13, HOUR_STRING_EXCEPTION, HOUR_NUMBER_EXCEPTION);
+            checkData(hour, 0, 23, HOUR_CHECK_EXCEPTION);
 
-            try {
-                String minuteString = dateAndTimeString.substring(14, 16);
-                minute = Integer.parseInt(minuteString);
-            } catch (StringIndexOutOfBoundsException e) {
-                throw new IllegalArgumentException("Minute string is less than 2 characters");
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Minute is not an integer");
-            }
-            if (minute < 0 || minute > 59)
-                throw new IllegalArgumentException("Minute cannot be less than 0 or more than 59");
-
+            minute = getNumFromDataAndTimeString(14, 16, MINUTE_STRING_EXCEPTION, MINUTE_NUMBER_EXCEPTION);
+            checkData(minute, 0, 59, MINUTE_CHECK_EXCEPTION);
         }
 
         Calendar calendar = Calendar.getInstance();
@@ -95,5 +69,21 @@ public class DateParser {
         calendar.set(year, month - 1, date, hour, minute, 0);
         calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
+    }
+
+    public int getNumFromDataAndTimeString(int startIndex, int endIndex, String stringException, String numberException) {
+        try {
+            String string = dateAndTimeString.substring(startIndex, endIndex);
+            return Integer.parseInt(string);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(stringException);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(numberException);
+        }
+    }
+
+    public void checkData(int data, int start, int end, String checkException) {
+        if (data < start || data > end)
+            throw new IllegalArgumentException(checkException);
     }
 }
